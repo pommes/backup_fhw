947a86bf90801917a1871ad343b793c59bd93830
pommes <pommerening@onlinehome.de>
2014-05-02 09:39:55 +0200

    Create config.cmd

---
+++ config.cmd
@@ -0,0 +1,9 @@
+:: Konfiguration für "backup_fhw"
+
+:: Debug-Modus: 1=An, 0=Aus
+set DEBUG=0
+:: Dienstnamen der Dienste die beim Backup gestoppt werden sollen
+set SRVNAME_HTTP=Apache2.2
+set SRVNAME_DB=MySQL
+set SRVNAME_FTP=FileZilla Server FTP server
+set SEVEN_ZIP_BIN="C:\Programme\7-Zip\7z.exe"
